3771c1e8f903fbd2b3ab2144746338107dbbe084
WillyFFS <willyfrans13@gmail.com>
2024-06-06 12:22:04 +0700

    login and register api

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,35 +1,49 @@
 const bcrypt = require("bcrypt");
-const { firestore , auth} = require("firebase-admin");
+const { db, auth } = require("../Config/firebaseConfig");
+const { collection, getDocs, addDoc, query, where } = require("@firebase/firestore/lite");
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config();
 
 async function RegisterHandler(request, h) {
     const { name, email, password } = request.payload;
-    let response;
-    console.log
+
     try {
+        const users = collection(db, 'Users');
+        
+        const q = query(users, where("email", "==", email));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+            return h.response({
+                error: true,
+                message: "Email Already In Use"
+            });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        console.log(firestore);
-        const newUser = await firestore.collection("Users").where('email',"==",email).get();
-        console.log("sebelum add collection");
-        console.log
-        await firestore.collection('Users').add({
+
+        const docRef = await addDoc(users, {
             name: name,
             email: email,
-            password: hashedPassword  
+            password: hashedPassword
         });
-        response = h.response({
+
+        console.log('Document written with ID:', docRef.id);
+
+        return h.response({
             error: false,
-            message: "User Created"
+            message: "User created"
         });
-            
+        
     } catch (error) {
-        console.error('Error registering user:', error);
-        response = h.response({
+        console.error('Error adding user:', error);
+        
+        return h.response({
             error: true,
-            message: "Email Already In Use",
+            message: error.message
         });
     }
-
-    return response;
 }
 
 async function LoginHandler(request, h) {
@@ -37,9 +51,12 @@ async function LoginHandler(request, h) {
     let response;
 
     try {
-        const userSnapshot = await firestore.collection('Users').where('email', '==', email).get();
-        if (!userSnapshot.empty) {
-            const userData = userSnapshot.docs[0].data();
+        const users = collection(db, 'Users');
+        
+        const q = query(users, where("email", "==", email));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+            const userData = querySnapshot.docs[0].data();
             const passwordMatch = await bcrypt.compare(password, userData.password);
 
             if (!passwordMatch) {
@@ -48,9 +65,15 @@ async function LoginHandler(request, h) {
                     message: "Check Your Password"
                 });
             } else {
-                const userCredential = await auth.signInWithEmailAndPassword(email, password);
-                const user = userCredential.user;
-                const token = await user.getIdToken();
+                const token = jwt.sign(
+                    {
+                        name: userData.name,
+                        email: userData.email
+                    },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1h' }
+                );
+
                 response = h.response({
                     error: false,
                     message: "Success",
@@ -79,12 +102,10 @@ async function LoginHandler(request, h) {
 }
 
 async function tesHandler(request,h){
-    console.log(firestore);
-    console.log("----------");
-    console.log(auth);
+    const { email, password } = request.payload;
     let response = h.response({
-        data: firestore,
-        data2: auth
+        email: email,
+        data: firestore 
     })
     return response
 }
@@ -94,4 +115,3 @@ module.exports = {
     LoginHandler,
     tesHandler
 };
-

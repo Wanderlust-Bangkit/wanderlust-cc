6233834ea82f1c8362294cccab8351e355d128f7
WillyFFS <willyfrans13@gmail.com>
2024-06-11 15:10:25 +0700

    adding 4 api

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const { db, auth } = require("../Config/firebaseConfig");
-const { collection, getDocs, addDoc, query, where } = require("@firebase/firestore/lite");
+const { collection, getDocs, addDoc, query, where ,doc, getDoc, setDoc, deleteDoc} = require("@firebase/firestore/lite");
 const jwt = require("jsonwebtoken");
 
 require('dotenv').config();
@@ -57,6 +57,7 @@ async function LoginHandler(request, h) {
         const querySnapshot = await getDocs(q);
         if (!querySnapshot.empty) {
             const userData = querySnapshot.docs[0].data();
+            const uid = querySnapshot.docs[0].id;
             const passwordMatch = await bcrypt.compare(password, userData.password);
 
             if (!passwordMatch) {
@@ -70,14 +71,14 @@ async function LoginHandler(request, h) {
                         name: userData.name,
                         email: userData.email
                     },
-                    process.env.JWT_SECRET,
-                    { expiresIn: '1h' }
+                    process.env.JWT_SECRET
                 );
 
                 response = h.response({
                     error: false,
                     message: "Success",
                     loginResult: {
+                        userId: uid,
                         name: userData.name,
                         email: userData.email,
                         token: token
@@ -101,17 +102,105 @@ async function LoginHandler(request, h) {
     return response;
 }
 
-async function tesHandler(request,h){
-    const { email, password } = request.payload;
-    let response = h.response({
-        email: email,
-        data: firestore 
-    })
+async function getDestinationHandler(request,h){
+    let response;
+    try {
+        const destination = collection(db, 'destination');
+        const querySnapshot = await getDocs(destination);
+        const data= [];
+        querySnapshot.forEach((doc)=>{
+            data.push({ id: doc.id, ...doc.data() });
+        })
+        response = h.response({
+            error: false,
+            data :  data
+        })
+    } catch(error){
+        console.error('Error get destination:', error);
+        response = h.response({
+            error: true,
+            message: error.message
+        });
+    }
     return response
+}
+
+async function addFavorit(request , h){
+    const { userId, destinationId } = request.payload;
+    let response;
+    try {
+        const destinationRef = doc(db,'destination',destinationId);
+        const destinationDoc = await getDoc(destinationRef);
+        console.log(destinationDoc.data());
+
+        const favoritColRef = collection(db,'Users',userId, 'Favorites');
+        await setDoc(doc(favoritColRef, destinationId), destinationDoc.data());
+
+        response = h.response({
+            error: false,
+            message: "Adding to favorite"
+        });
+    } catch (error) {
+        response = h.response({
+            error: true,
+            message: error.message
+        });
+    }
+    return response;
+}
+
+async function getFavorit(request,h){
+    const  userId = request.params.userId;
+    let response;
+    try {
+        const favoritColRef = collection(db,'Users',userId, 'Favorites');
+        const favorite = await getDocs(favoritColRef);
+        const data= [];
+        favorite.forEach((doc)=>{
+            data.push({ id: doc.id, ...doc.data() });
+        })
+
+        response = h.response({
+            error: false,
+            data: data
+        });
+    } catch (error) {
+        response = h.response({
+            error: true,
+            message: error.message
+        });
+    }
+    return response;
+}
+
+async function deleteFavorit(request,h){
+    const { userId, destinationId } = request.payload;
+    let response;
+    try{
+        console.log('1');
+        const favoritColRef = collection(db, 'Users', userId, 'Favorites');
+        console.log('a');
+        await deleteDoc(doc(favoritColRef, destinationId));
+        console.log('2');
+        response = h.response({
+            error: true,
+            message: "Delete favorite"
+        });
+    } catch (error){
+        response = h.response({
+            error: true,
+            message: error.message
+        });
+    }
+
+    return response;
 }
 
 module.exports = {
     RegisterHandler,
     LoginHandler,
-    tesHandler
+    getDestinationHandler,
+    addFavorit,
+    getFavorit,
+    deleteFavorit
 };
